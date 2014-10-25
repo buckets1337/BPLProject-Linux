4a6288a3c10867b979f2c73e68e2f82960e7aabc
Justin Lomelino <justinl@bloomingtonlibrary.org>
2014-10-25 11:19:56 -0500

    Initial Commit.  Includes all the files for the Linux version of BPLProject

---
+++ CONFIG.py
@@ -0,0 +1,29 @@
+#CONFIG.py
+#handles configuration variables
+
+SCREEN_SIZE = width,height = 1024,768
+IMAGE_PATH = 'Pictures/'
+WEAPON_PATH = 'Weapons'
+PIXEL_ARRAY_PATH = 'pixelArrays'
+RED_TEAM_PATH = 'redTeam.txt'
+BLUE_TEAM_PATH = 'blueTeam.txt'
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+DARK_RED = (128,0,0)
+ORANGE = (255,153,0)
+GREEN = (0,255,0)
+DARK_GREEN = (0,128,0)
+BLUE = (0,0,255)
+DARK_BLUE = (0,0,128)
+
+
+HEAT_LIMIT = 10.0
+BASE_EVASION = 35.0
+
+
+##### WEAPON REGISTER #####
+# weapons must be in this list to be used in the game.
+WEAPON_REGISTER = [None, 'SlugGun', 'MachineGun', 'Mortar']
+
---
+++ IDs.py
@@ -0,0 +1,25 @@
+#IDs.py
+#lists information about each player to be loaded into the game.
+
+#each entry for a player should be a list, with attributes in this order:
+#1. player name
+#2. avatar image filename
+#3. pixelArray to use, instead of an image.
+
+#NOTE: THIS WILL CHANGE ONCE PIXEL ARRAYS ARE USED FOR PLAYER IMAGES
+
+playerList = (
+
+['justin', 'players/Justin_Lomelino.jpg', None],
+['john', 'players/Jon_Whited_IT_Manager.jpg', None],
+['kerrie', 'players/Kerrie_Parker_Webmaster.jpg', None],
+['joe', None, 'demoPixelArray'],
+['bill', None, None],
+['Paul', 'players/Paul_Short.jpg', None],
+['henry', 'players/Henry_Birmingham.jpg', None],
+['devon', 'players/Volunteer_Devon_Phifer.jpg', None],
+['tina', None, None],
+['claudette', None, None],
+['dog?', None, None]
+
+)
